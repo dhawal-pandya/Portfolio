81d36fa295814bd5156546c15def53929bbf988c
Dhawal Pandya <dhawalpandya.01@gmail.com>
2022-07-19 00:34:05 +0530

    Uploading the source code

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaAward } from 'react-icons/fa';
 import { VscFolderLibrary } from 'react-icons/vsc';
-import ME from '../../assets/me.jpg';
+import ME from '../../assets/me.png';
 import './intro.css';
 
 const Intro = () => {
